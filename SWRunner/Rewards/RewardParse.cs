c26264edcb1a7ded80a12bad1259032ff3744dde
HV <hoavuong1991@gmail.com>
2019-09-27 17:06:45 -0700

    Added reward parser

---
+++ SWRunner/Rewards/RewardParse.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SWRunner.Rewards
+{
+    public static class RewardParse
+    {
+        public static Reward GetReward(string path)
+        {
+            return null;
+        }
+
+        public static Reward GetReward(string path, int row)
+        {
+            return null;
+        }
+
+    }
+}
