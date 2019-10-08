0e041b912d3d6b3e18744dd29876553239642dc5
HV <hoavuong1991@gmail.com>
2019-10-08 12:57:28 -0700

    Reworked on SWRunnerApp with some basic setup

---
+++ SWRunnerApp/MainWindow.xaml.cs
@@ -0,0 +1,79 @@
+﻿using SWRunner.Runners;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace SWRunnerApp
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private SWRunnerPresenter Present { get; } = new SWRunnerPresenter();
+
+        private CancellationTokenSource tokenSource;
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private async void button_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show(ConfigurationManager.AppSettings["CairosRunnerConfig"]);
+
+            if (tokenSource != null)
+            {
+                tokenSource.Cancel();
+            }
+            else
+            {
+                tokenSource = new CancellationTokenSource();
+                ((Button)sender).Content = "Cancel";
+                try
+                {
+                    await DoWorkAsyncInfiniteLoop(tokenSource.Token);
+                }
+                catch (OperationCanceledException)
+                {
+                    log.Text += "Canceled";
+                }
+            }
+
+            tokenSource = null;
+        }
+
+        private async Task DoWorkAsyncInfiniteLoop(CancellationToken ct)
+        {
+            while (true)
+            {
+                // do the work in the loop
+                string newData = DateTime.Now.ToLongTimeString();
+
+                // update the UI
+                log.Text += newData + Environment.NewLine;
+
+                // don't run again for at least 200 milliseconds
+                await Task.Delay(1000, ct);
+            }
+        }
+
+        private void Log_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            log.ScrollToEnd();
+        }
+    }
+}
---
+++ SWRunnerApp/SWRunnerPresenter.cs
@@ -0,0 +1,42 @@
+﻿using SWEmulator;
+using SWRunner.Filters;
+using SWRunner.Runners;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.IO;
+using System.Text;
+using System.Threading;
+using System.Xml.Serialization;
+
+namespace SWRunnerApp
+{
+    class SWRunnerPresenter
+    {
+        public CairosRunner CairosRunner { get; private set; }
+
+        public bool Stop { get; set; } = false;
+
+        public SWRunnerPresenter()
+        {
+            InitCairosRunner();
+        }
+
+        private void InitCairosRunner()
+        {
+            XmlSerializer serializer = new XmlSerializer(typeof(CairosRunnerConfig), new XmlRootAttribute("RunConfig"));
+            string testConfigXml = ConfigurationManager.AppSettings["CairosRunnerConfig"];
+            CairosRunnerConfig runConfig;
+
+            using (Stream reader = new FileStream(testConfigXml, FileMode.Open))
+            {
+                // Call the Deserialize method to restore the object's state.
+                runConfig = (CairosRunnerConfig)serializer.Deserialize(reader);
+            }
+
+            // TODO
+            CairosRunner = new CairosRunner(new CairosFilter(), "", runConfig, new NoxEmulator(""));
+        }
+
+    }
+}
